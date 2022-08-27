366f915e2d507501c2dbfd9b995b08b6fc1d2ddf
Daniel Baker <daniel.baker@tweag.io>
2022-08-27 11:00:29 -0700

    Changed how text parsing works and downstream code.

--- convert_quests.py
+++ convert_quests.py
@@ -8,11 +8,13 @@ from pprint import pprint
 translator = Translator(service_urls=['translate.google.com'])
 
 
-def get_title_and_text(quest: str) -> Tuple[str, List[str]]:
-    title_line = re.findall(r'title: "(.*)"', quest)[0]
-    text_lines = re.findall(r'text: \[\n([^]]*)\n\t\]', quest)[0]
-    text_lines = re.findall(r'"(.+)"', text_lines)
-    return title_line, text_lines
+def get_text_fields(quest: str) -> Tuple[str]:
+    title_search = re.search(r'title: "(.*)"', quest)
+    title_line = (title_search.group(1),) if title_search else ()
+
+    text_search = re.search(r'text: \[\n([^]]*)\n\t\]', quest)
+    text_lines = re.findall(r'"(.+)"', text_search.group(1)) if text_search else ()
+    return (*title_line, *text_lines)
 
 
 def translate_line(line: str) -> str:
@@ -48,27 +50,27 @@ def translate_line(line: str) -> str:
         return translate_line(line)
 
 
-def update_quest(quest: str) -> str:
-    title, text_lines = get_title_and_text(quest=quest)
-    quest = quest.replace(title, translate_line(title))
-    for line in text_lines:
+def update_quest(quest: str, text_fields: Tuple[str]) -> str:
+    for line in text_fields:
         quest = quest.replace(line, translate_line(line))
     return quest
 
 
 def update_quest_file(input_path: Path, output_path: Path) -> None:
     with open(input_path, 'r') as fin:
-        raw_text = fin.read()
-        if raw_text.find('title') == -1 or raw_text.find('text') == -1:
-            print(f'No text or title found in {input_path}')
-            print(raw_text)
-            new_text = raw_text
+        quest = fin.read()
+        text_fields = get_text_fields(quest)
+        if not text_fields:
+            print(f'No text or title found in {input_path}.')
+            print(quest)
         else:
-            print(f'Translating {input_path}')
-            new_text = update_quest(raw_text)
+            # print(text_fields)
+            print(f'Translating {input_path}.')
+        new_text = update_quest(quest, text_fields)
 
     with open(output_path, 'w') as fout:
         fout.write(new_text)
+
 
 def make_output_path(path: Path) -> Path:
     parts = list(path.parts)
@@ -83,6 +85,12 @@ def main():
     for input_path in quest_path.rglob("*.snbt"):
         output_path = make_output_path(input_path)
         update_quest_file(input_path, output_path)
+
+    # with open(Path("chapters/4b5af78b/1f9e8255.snbt"), 'r') as fin:
+    # # with open(Path("chapters/4b5af78b/chapter.snbt"), 'r') as fin:
+    #     raw_text = fin.read()
+    #     pprint(get_text_fields(raw_text))
+    #     print(raw_text)
 
 
 if __name__ == '__main__':
